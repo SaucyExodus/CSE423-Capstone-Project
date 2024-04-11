75bc6e532be740d81d4a2976f6df852d43e3747a
Gage Cates <gtcates@asu.edu>
2024-04-11 15:00:19 -0700

    Started up Home UI.

--- functions/listeners/events/app_home_opened.js
+++ functions/listeners/events/app_home_opened.js
@@ -1,15 +1,93 @@
 
-const appHomeOpenedCallback = async ({ event, say }) => {
+const appHomeOpenedCallback = async ({ event, client }) => {
     if (event.tab !== 'home'){
         // Only sends responds on app home.
         return;
     }
 
-    // Send message to user when open app. 
-    // Place holder as we will add the UI by using this event.
-    say(`Hello world and <@${event.user}>!`)
+    try {
 
+        const result = client.views.publish({
 
+            user_id: event.user,
+
+            view: {
+                type: 'home',
+                callback_id: 'home_view',
+
+                blocks: [
+                    {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Here's what you can do with Project Tracker:"
+                        }
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Create New Task",
+                                    "emoji": true
+                                },
+                                "style": "primary",
+                                "value": "create_task"
+                            },
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Create New Project",
+                                    "emoji": true
+                                },
+                                "value": "create_project"
+                            },
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Help",
+                                    "emoji": true
+                                },
+                                "value": "help"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "image",
+                                "image_url": "https://api.slack.com/img/blocks/bkb_template_images/placeholder.png",
+                                "alt_text": "placeholder"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "*Your Configurations*"
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "divider"
+                    }
+
+                ]
+            }
+        });
+    }
+
+    catch (error) {
+        console.error(error);
+    }
 };
 
 module.exports = { appHomeOpenedCallback };
