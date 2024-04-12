f74c3ce21cbeff0af55763be7aa931d0fe18ef48
Gage Cates <gtcates@asu.edu>
2024-04-12 15:12:09 -0700

    Create the task modal UI

--- functions/listeners/index.js
+++ functions/listeners/index.js
@@ -1,5 +1,7 @@
 const eventsListener = require('./events');
+const commandsListener = require('./commands');
 
 module.exports.registerListeners = (app) => {
     eventsListener.register(app);
+    commandsListener.register(app);
 };
